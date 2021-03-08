5b7a30171535791eb1218c8ef01d459934be5598
Cody Dennis <codydennis0@gmail.com>
2021-03-08 18:15:55 -0500

    TODO

---
+++ Configurator/Characterizer.py
@@ -0,0 +1,25 @@
+"""
+Landscape Aware Algorithm Configurator
+Copyright (C) 2021 Cody Dennis
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+""" 
+Responsible for producing a feature vector from a sequence of solutions.
+"""
+
+#TODO: once the uniform sampler and configDB are done, make a characterizer from your c++ code and wrap it here 
+class Characterizer:
+    pass
