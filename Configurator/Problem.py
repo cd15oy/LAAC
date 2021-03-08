7253ab97de5b26ec01711cc18f2d365a465c450b
Cody Dennis <codydennis0@gmail.com>
2021-03-08 18:17:36 -0500

    typing annotations

--- Configurator/Problem.py
+++ Configurator/Problem.py
@@ -21,9 +21,13 @@ A class representing a general problem
 """
 
 from random import Random 
+from typing import List
 
+"""
+Represents a specific instance of a problem 
+"""
 class Problem:
-    def __init__(self, name, flag, instances, seed):
+    def __init__(self, name:str, flag:str, instances:List[str], seed:int):
         self.rng = Random(seed)
         self.instanceTemplates = instances
         self.name = name 
@@ -31,46 +35,43 @@ class Problem:
         self.instances = set()
 
     #produces the flags needed by the aglorithm to use the problem 
-    def toFlags(self):
+    def toFlags(self)->str:
         return self.flag
 
     #get n instances chosen uniformly at random from existing instances (with replacement) 
-    def sampleInstances(self, n):
+    def sampleInstances(self, n:int)->List["Instance"]:
         #TODO: choices(list(set)) is inefficent, consider doing something less stupid
         return self.rng.choices(list(self.instances), k=n)
     
     #generate a concrete instance from the template 
     #if the instance is a static set of flags, it is returned as is, otherwise any keywords are replaced as needed. ex $RANDOM is replaced with a random seed 
-    def _parseTemplate(self, template):
+    def _parseTemplate(self, template:str) -> str:
         #nothing fancy, just a replace for each possible keyword 
         ret = template.replace("$RANDOM", str(self.rng.randint(0,4000000000)))
         return ret  
 
     #Generate a new instances from a random template
     #The new instance is added to the set of instances 
-    def generateInstance(self):
+    def generateInstance(self) -> "Instance":
         rndTemplate = self.rng.choice(self.instanceTemplates)
         instanceString = self._parseTemplate(rndTemplate) 
         instance = Instance(self, instanceString) 
         self.instances.add(instance) 
         return instance 
 
-"""
-Represents a specific instance of a problem 
-"""
 class Instance:
-    def __init__(self, problem, instanceData):
+    def __init__(self, problem:Problem, instanceData:str):
         self.problem = problem 
         self.flags = instanceData 
 
-    def toFlags(self):
+    def toFlags(self)->str:
         return "{0} {1}".format(self.problem.toFlags(), self.flags)
 
-    def __hash__(self):
+    def __hash__(self)->int:
         return self.toFlags().__hash__()
 
-    def __eq__(self, other):
+    def __eq__(self, other:"Instance") -> bool:
         return self.__hash__() == other.__hash__()
 
-    def __ne__(self, other):
+    def __ne__(self, other:"Instance") -> bool:
         return self.__eq__(other) == False 
